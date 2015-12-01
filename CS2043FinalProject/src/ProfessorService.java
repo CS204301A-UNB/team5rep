058314190a76214542debc06bcae1cec653f7d21
JoshuaKirby <Joshua.Kirby@Outlook.com>
2015-12-01 17:01:19 -0400

    Added Skeleton Methods

--- CS2043FinalProject/src/ProfessorService.java
+++ CS2043FinalProject/src/ProfessorService.java
@@ -1,4 +1,21 @@
 
 public class ProfessorService {
-
+	public void processPostGrades(){
+		
+	}
+	public void processPostCourseInfo(){
+		
+	}
+	public void processManageDropBox(){
+		
+	}
+	public void processCreateQuiz(){
+		
+	}
+	public void processPostSelfAssignment(){
+		
+	}
+	public void processPostSurvey(){
+		
+	}
 }
--- CS2043FinalProject/src/StudentService.java
+++ CS2043FinalProject/src/StudentService.java
@@ -1,4 +1,21 @@
 
 public class StudentService {
-
+	public void processViewGrade(){
+		
+	}
+	public void processAnswerSurvey(){
+		
+	}
+	public void processAnswerQuiz(){
+		
+	}
+	public void processViewCourseInfo(){
+		
+	}
+	public void processSubmitAssignment(){
+		
+	}
+	public void processAnswerSelfAssessment(){
+		
+	}
 }
