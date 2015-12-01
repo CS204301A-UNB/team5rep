53f74bd8ef7df6ef81ca8af2d57a619b151326e2
rcreagha <r_creaghan@hotmail.com>
2015-12-01 17:06:08 -0400

    fix commit

--- CS2043Lab10/src/InfoApp.java
+++ CS2043Lab10/src/InfoApp.java
@@ -5,9 +5,6 @@ public class InfoApp {
 	public static void main(String[] args) {
 		System.out.println("Team 5 Member List:");
 		
-		InfoPrintCreaghan rc = new InfoPrintCreaghan();
-		rc.printCreaghan();
-
 	}
 
 }
